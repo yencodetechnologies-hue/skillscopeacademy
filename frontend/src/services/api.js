86867bcaacf590419047849be572d45e59d48566
Jayaramramarajaguru <jram20902@gmail.com>
2026-06-04 19:27:39 +0530

    modified

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: "https://skillscopeacademy.yencodetechnologies.in/api" || "http://localhost:7000/api"
+  baseURL: "https://skillscopeacademy.yencodetechnologies.in/api"
 })
 
 /*

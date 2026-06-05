f418b3feedfe63dab66310f74c94b5bd1995b394
Jayaramramarajaguru <jram20902@gmail.com>
2026-06-05 10:46:18 +0530

    modified

--- backend/server.js
+++ backend/server.js
@@ -34,6 +34,7 @@ app.use(cors({
   credentials: true,
 }))
 
+
 // ── Routes ───────────────────────────────────────────────────
 app.use('/api/auth',           authRoutes)
 app.use('/api/courses',        courseRoutes)
